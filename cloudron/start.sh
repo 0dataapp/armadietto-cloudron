7686114d44512592980a6cb1d6bee11e5432fa05
Rosano Coutinho <github.com@xeari.com>
2025-10-20 14:07:23 +0100

    update cloudron config

---
+++ cloudron/start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# ensure that this file executable in the app repo: chmod +x start.sh.
+
+if [[ ! -f /app/data/conf.json ]]; then
+  cp /app/code/bin/conf.monolithic.json /app/data/conf.json
+
+  sed -i'.bak' -e 's/"allow_signup": false/"allow_signup": true/g' /app/data/conf.json
+  sed -i'.bak' -e 's/"storage_path": "\/usr\/share\/armadietto"/"storage_path": "\/app\/data\/armadietto"/g' /app/data/conf.json
+  sed -i'.bak' -e 's/"log_dir": "logs"/"log_dir": "\/app\/data\/logs"/g' /app/data/conf.json
+  
+  rm /app/data/conf.json.bak
+fi
+
+chown -R cloudron:cloudron /app/data
+
+mkdir -p /app/data/armadietto
+mkdir -p /app/data/logs
+
+exec /usr/local/bin/gosu cloudron:cloudron /app/code/bin/armadietto.js -c /app/data/conf.json
