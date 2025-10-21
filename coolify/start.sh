ce66fb260e0874c806ec821b6c990a7c0c8128be
Rosano Coutinho <github.com@xeari.com>
2025-10-21 09:28:19 +0100

    switch to armadietto user

--- coolify/start.sh
+++ coolify/start.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
+su - armadietto
+
 if [[ "$ALLOW_SIGNUP" == "false" ]]; then
-  sudo sed -i 's/"allow_signup": true/"allow_signup": false/' /etc/armadietto/conf.json
+  sed -i 's/"allow_signup": true/"allow_signup": false/' /etc/armadietto/conf.json
 fi
 
 if [[ "$ALLOW_SIGNUP" != "false" ]]; then
-  sudo sed -i 's/"allow_signup": false/"allow_signup": true/' /etc/armadietto/conf.json
+  sed -i 's/"allow_signup": false/"allow_signup": true/' /etc/armadietto/conf.json
 fi
 
 armadietto -c /etc/armadietto/conf.json
