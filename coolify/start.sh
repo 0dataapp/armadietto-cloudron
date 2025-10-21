59bd3718ea2ae4f6babbac9276b93a8c636f013c
Rosano Coutinho <github.com@xeari.com>
2025-10-21 09:24:20 +0100

    remove sed backup

--- coolify/start.sh
+++ coolify/start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if [[ "$ALLOW_SIGNUP" == "false" ]]; then
-  sed -i'.bak' -e 's/"allow_signup": true/"allow_signup": false/g' /etc/armadietto/conf.json
+  sed -i 's/"allow_signup": true/"allow_signup": false/' /etc/armadietto/conf.json
 fi
 
 if [[ "$ALLOW_SIGNUP" != "false" ]]; then
-  sed -i'.bak' -e 's/"allow_signup": false/"allow_signup": true/g' /etc/armadietto/conf.json
+  sed -i 's/"allow_signup": false/"allow_signup": true/' /etc/armadietto/conf.json
 fi
 
 armadietto -c /etc/armadietto/conf.json
