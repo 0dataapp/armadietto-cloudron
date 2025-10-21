1a192bc380ed4b84123ebb01bcd4a81e92e26dc9
Rosano Coutinho <github.com@xeari.com>
2025-10-21 09:12:01 +0100

    skip copy

--- coolify/start.sh
+++ coolify/start.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-echo "hello"
-
-cp -f /opt/armadietto/bin/conf.monolithic.json /etc/armadietto/conf.json
-
 if [[ "$ALLOW_SIGNUP" == "false" ]]; then
   sed -i'.bak' -e 's/"allow_signup": true/"allow_signup": false/g' /etc/armadietto/conf.json
-  rm /etc/armadietto/conf.json.bak
 fi
 
-ln -s /opt/armadietto/bin/armadietto.js /usr/local/bin/armadietto
+if [[ "$ALLOW_SIGNUP" != "false" ]]; then
+  sed -i'.bak' -e 's/"allow_signup": false/"allow_signup": true/g' /etc/armadietto/conf.json
+fi
+
 armadietto -c /etc/armadietto/conf.json
